aba56eab842a12e99a79299e226d1bfbb63cfe91
vsevaq <vsevolodq@gmail.com>
2023-01-26 21:35:38 +0200

    Angela. Section 2. I Am Rich App

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Vsevolod Honcharenko on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
